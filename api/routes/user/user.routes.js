560d1ce4626d0bd586db6aa5c41c581067324abc
Matheus Oliveira <olivermathe16@gmail.com>
2018-06-30 23:16:35 -0300

    Ident with space

--- api/routes/user/user.routes.js
+++ api/routes/user/user.routes.js
@@ -2,45 +2,45 @@ const Handler = require('./user.Handler');
 const Joi = require('joi');
 
 module.exports = [
-	{
-		method: 'PUT',
-		path: '/user/create',
-		handler: (request, reply) => Handler.create(request.payload, reply),
-		config: {
-			description: 'Create new user',
-			// auth: 'jwt',
-			validate: {
-				payload: {
-					name: Joi.string().required(),
-					pwd: Joi.string().required(),
-					email: Joi.string().email().required()
-				}
-			}
-		}
-	},
-	{
-		method: 'POST',
-		path: '/user/login',
-		handler: (request, reply) => Handler.login(request.payload, reply),
-		config: {
-			description: 'Login',
-			auth: false,
-			validate: {
-				payload: {
-					pwd: Joi.string().required(),
-					email: Joi.string().email().required()
-				}
-			}
-		}
-	},
-	{
-		method: 'GET',
-		path: '/user/{id}',
-		handler: (request, reply) => Handler.getById(request.params.id, reply),
-		config: {
-			description: 'Create new user',
-			auth: 'jwt'
-		}
-	}
+    {
+        method: 'PUT',
+        path: '/user/create',
+        handler: (request, reply) => Handler.create(request.payload, reply),
+        config: {
+            description: 'Create new user',
+            // auth: 'jwt',
+            validate: {
+                payload: {
+                    name: Joi.string().required(),
+                    pwd: Joi.string().required(),
+                    email: Joi.string().email().required()
+                }
+            }
+        }
+    },
+    {
+        method: 'POST',
+        path: '/user/login',
+        handler: (request, reply) => Handler.login(request.payload, reply),
+        config: {
+            description: 'Login',
+            auth: false,
+            validate: {
+                payload: {
+                    pwd: Joi.string().required(),
+                    email: Joi.string().email().required()
+                }
+            }
+        }
+    },
+    {
+        method: 'GET',
+        path: '/user/{id}',
+        handler: (request, reply) => Handler.getById(request.params.id, reply),
+        config: {
+            description: 'Create new user',
+            auth: 'jwt'
+        }
+    }
 
 ];
